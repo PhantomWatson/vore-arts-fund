eb2aeea96ed2f3a59f92140d499326cf24cd2c97
Graham Watson <graham@phantomwatson.com>
2020-02-19 16:16:43 -0500

    Remove doc comment for method in different class

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -34,17 +34,6 @@ use Cake\Mailer\Email;
  */
 class UsersController extends AppController
 {
-
-    /**
-     * Displays a view
-     *
-     * @param array ...$path Path segments.
-     * @return \Cake\Http\Response|null
-     * @throws \Cake\Http\Exception\ForbiddenException When a directory traversal attempt.
-     * @throws \Cake\Http\Exception\NotFoundException When the view file could not
-     *   be found or \Cake\View\Exception\MissingTemplateException in debug mode.
-     */
-
     protected $name = 'Users';
     protected $helpers = array('Html', 'Form', 'Time');
     protected $uses = array('User');
