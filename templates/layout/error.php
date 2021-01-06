5fc24b69d3274f79fe710b9ffb3eb2b6f5e06665
Graham Watson <graham@phantomwatson.com>
2021-01-06 12:30:07 -0500

    Fix error layout not echoing anything

--- templates/layout/error.php
+++ templates/layout/error.php
@@ -3,6 +3,7 @@
  * @var \App\View\AppView $this
  */
 
-$this->extend('layout/default');
 $this->assign('footer', $this->Html->link(__('Back'), 'javascript:history.back()'));
 $this->assign('title', 'Error');
+$this->extend('default');
+echo $this->fetch('content');
