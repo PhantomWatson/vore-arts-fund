251a56860c3c7659d08290f242f764e584ee8b38
Graham Watson <graham@phantomwatson.com>
2021-09-10 00:13:41 -0400

    Fix indentation

--- templates/Users/login.php
+++ templates/Users/login.php
@@ -5,24 +5,24 @@
 ?>
 
 <div class="pb-2 mt-4 mb-2 border-bottom">
-  <h1>Login</h1>
+    <h1>Login</h1>
 </div>
 
 <div class="users form">
-  <?= $this->Flash->render() ?>
-  <?= $this->Form->create() ?>
-  <fieldset>
-      <?= $this->Form->control('email') ?>
-      <?= $this->Form->control('password') ?>
-  </fieldset>
-  <?= $this->Form->submit(__('Login'), ['class' => 'btn btn-primary']) ?>
-  <?= $this->Form->end() ?>
-  <?= $this->Html->link(
-      'Forgot Password?',
-      [
-          'controller' => 'Users',
-          'action' => 'forgotPassword',
-      ],
-      ['class' => 'btn btn-secondary']
-  ) ?>
+    <?= $this->Flash->render() ?>
+    <?= $this->Form->create() ?>
+    <fieldset>
+        <?= $this->Form->control('email') ?>
+        <?= $this->Form->control('password') ?>
+    </fieldset>
+    <?= $this->Form->submit(__('Login'), ['class' => 'btn btn-primary']) ?>
+    <?= $this->Form->end() ?>
+    <?= $this->Html->link(
+        'Forgot Password?',
+        [
+            'controller' => 'Users',
+            'action' => 'forgotPassword',
+        ],
+        ['class' => 'btn btn-secondary']
+    ) ?>
 </div>
