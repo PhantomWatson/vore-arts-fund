8a5dd9244afe336d0a72ba3d383b2dacc2c2c9f6
Graham Watson <graham@phantomwatson.com>
2022-03-08 00:30:47 -0500

    Update /register copy and styling

--- templates/Users/register.php
+++ templates/Users/register.php
@@ -13,20 +13,27 @@
 </div>
 
 <div class="users form" id="register-form">
+    <p>
+        Register an account in order to vote or to submit applications for funding
+    </p>
     <?= $this->Form->create($user) ?>
     <fieldset>
+        <?= $this->Form->control('name', ['id' => 'register-name']) ?>
+        <p class="footnote">
+            This can be your name or the name of your organization
+        </p>
         <?= $this->Form->control('email', ['type' => 'email']) ?>
         <?= $this->Form->control('password') ?>
-        <?= $this->Form->control('name') ?>
         <?= $this->Form->control('phone', [
             'type' => 'tel',
             'minLength' => 10,
             'label' => 'Phone number',
+            'placeholder' => 'Cell number with area code, e.g. 765-123-4567'
         ]) ?>
         <p class="footnote">
-            (with area code, e.g. 765-123-4567)
+            We only use this to verify your account and prevent multi-account abuse
         </p>
     </fieldset>
-    <?= $this->Form->submit(__('Submit'), ['class' => 'btn btn-primary']) ?>
+    <?= $this->Form->submit('Register', ['class' => 'btn btn-primary']) ?>
     <?= $this->Form->end() ?>
 </div>
