f584b396e8bc595345904d892d751fb23d0e64ad
Graham Watson <graham@phantomwatson.com>
2022-05-29 01:24:10 -0400

    Replace CDN bootstrap with downloaded file

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -29,9 +29,7 @@ $title = $title ?? $this->fetch('title');
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
             integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
     </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
-            integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
-    </script>
+    <script src="/js/bootstrap.min.js"></script>
     <link href="https://fonts.googleapis.com/css?family=Raleway:500i|Roboto:300,400,700|Roboto+Mono" rel="stylesheet">
 </head>
 <body>
