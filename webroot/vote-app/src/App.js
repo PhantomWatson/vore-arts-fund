2d389bdc6e6eaa31dad93e05858fd922214f614a
Graham Watson <graham@phantomwatson.com>
2022-07-18 01:34:27 -0400

    Set up navigation between steps

--- webroot/vote-app/src/App.js
+++ webroot/vote-app/src/App.js
@@ -1,15 +1,19 @@
-import * as React from 'react';
 import "./App.css";
-import ApplicationList from "./ApplicationList";
+import * as React from 'react';
 import API from "./API.js";
+import ApplicationList from "./ApplicationList";
+import Button from 'react-bootstrap/Button';
 import {useEffect, useState} from "react";
 
 const App = () => {
   const [applications, setApplications] = useState(null);
   const [currentStep, setCurrentStep] = useState('select');
   const [errorMsg, setErrorMsg] = useState(null);
-
+  const [allVotesAreCast, setAllVotesAreCast] = useState(false);
+  const [approvedApplications, setApprovedApplications] = useState([]);
+  const [sortingIsFinished, setSortingIsFinished] = useState(false);
   const handleVote = (applicationId, vote) => {
+    // Update the selected application's vote
     let applicationIsFound = false;
     for (let i = 0, length = applications.length; i < length; i++) {
       if (applications[i].id === applicationId) {
@@ -21,10 +25,42 @@ const App = () => {
       console.log(`Application #${applicationId} not found`);
     }
     setApplications(applications);
+
+    // Update list of approved applications
+    let approved = [];
+    for (let i = 0, length = applications.length; i < length; i++) {
+      if (applications[i].vote) {
+        approved.push(applications[i]);
+      }
+    }
+    setApprovedApplications(approved);
+
+    // Update whether or not all applications have been voted on
+    let pendingVoteFound = false;
+    for (let i = 0, length = applications.length; i < length; i++) {
+      if (applications[i].vote === null) {
+        pendingVoteFound = true;
+        break;
+      }
+    }
+    setAllVotesAreCast(!pendingVoteFound);
+  };
+  const handleGoToSelect = () => {
+    setCurrentStep('select');
+  };
+  const handleGoToSort = () => {
+    if (approvedApplications.length > 1) {
+      setCurrentStep('sort');
+    } else {
+      setCurrentStep('submit');
+    }
+  };
+  const handleGoToSubmit = () => {
+    setCurrentStep('submit');
   };
 
+  // Fetch applications and set their vote property to null (no vote cast)
   useEffect(async () => {
-    // Fetch applications and set their vote property to null (no vote cast)
     let fetchedApplications = await API.getApplications(setErrorMsg);
     if (fetchedApplications === null) {
       if (!errorMsg) {
@@ -61,33 +97,53 @@ const App = () => {
             <>
               <ul id="vote-steps">
                 <li className={currentStep === 'select' ? 'active' : ''}>
-                 1. Select
+                  <Button variant="link" disabled={!allVotesAreCast} onClick={handleGoToSelect}>
+                    1. Select
+                  </Button>
                 </li>
                 <li className={currentStep === 'sort' ? 'active' : ''}>
-                  2. Sort
+                  <Button variant="link" disabled={!allVotesAreCast} onClick={handleGoToSort}>
+                    2. Sort
+                  </Button>
                 </li>
                 <li className={currentStep === 'submit' ? 'active' : ''}>
-                  3. Submit
+                  <Button variant="link" disabled={!(allVotesAreCast && (sortingIsFinished || approvedApplications.length < 2))} onClick={handleGoToSubmit}>
+                    3. Submit
+                  </Button>
                 </li>
               </ul>
               {currentStep === 'select' &&
                 <>
-                  <p>
+                  <p className="alert alert-info">
                     <span className="vote-step-title">Step one:</span> Review each application and either <strong>approve</strong> it
                     if you think it should be funded or <strong>reject</strong> it.
                   </p>
                   <ApplicationList applications={applications} handleVote={handleVote} />
+                  <div className="vote-footer">
+                    <Button disabled={!allVotesAreCast} variant="primary" size="lg" onClick={handleGoToSort}>
+                      Next
+                    </Button>
+                  </div>
                 </>
               }
               {currentStep === 'sort' &&
-                <p>
-                  Sort
-                </p>
+                <>
+                  <p className="alert alert-info">
+                    <span className="vote-step-title">Step two:</span> Now that you've <em>selected</em> the {' '}
+                    applications that you want funded, it's time to <strong>rank</strong> them, with #1 being the {' '}
+                    highest-priority for funding, and #{approvedApplications.length} being the lowest-priority.
+                  </p>
+                  <p>
+                    Applications approved
+                  </p>
+                </>
               }
               {currentStep === 'submit' &&
-                <p>
-                  Submit
-                </p>
+                <>
+                  <p>
+                    Submit
+                  </p>
+                </>
               }
             </>
           }
