0b0467096b7cf4ec2bbe146b520683df6b0802a1
Graham Watson <graham@phantomwatson.com>
2022-09-30 00:16:35 -0400

    Fix line breaks

--- templates/email/html/application_accepted.php
+++ templates/email/html/application_accepted.php
@@ -12,11 +12,12 @@
 </p>
 
 <p>
-    So what comes next? On <?= $fundingCycle->vote_begin->format('F j, Y') ?>, voting will begin for the applications
-    in this funding cycle. Then on <?= $fundingCycle->vote_end->format('F j, Y') ?>, the
+    So what comes next? On <?= $fundingCycle->vote_begin->format('F j, Y') ?>, voting will begin for the
+    applications in this funding cycle. Then on <?= $fundingCycle->vote_end->format('F j, Y') ?>, the
     voting period will end, and we'll email you soon afterward to let you know whether or not you'll be awarded funding.
 </p>
 
 <p>
-    Good luck, and be sure to tell your supporters to visit <a href="https://VoreArtsFund.org">VoreArtsFund.org</a> and vote for you!
+    Good luck, and be sure to tell your supporters to visit <a href="https://VoreArtsFund.org">VoreArtsFund.org</a> and
+    vote for you!
 </p>
