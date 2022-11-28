7abec6dfc3110f50d71d96e59d085c7672081158
Graham Watson <graham@phantomwatson.com>
2022-11-28 00:39:30 -0500

    Update about page

--- templates/Pages/about.php
+++ templates/Pages/about.php
@@ -15,135 +15,141 @@
  */
 ?>
 
-<article class="about">
-    <h2>
-        What is the Vore Arts Fund?
-    </h2>
-    <p>
-        The Vore Arts Fund is a 501(c)(3) not-for-profit company incorporated in 2020 to financially support the arts
-        and entertainment community of Muncie, Indiana. It distributes no-contract, no-interest loans to cover the
-        up-front costs of producing for-profit art, music, performances, and art education.
-    </p>
-    <p>
-        By absorbing the financial risk inherent in arts-related projects with up-front costs, this program makes it
-        easier for artists, educators, performers, and promoters to improve the quality-of-place of our community.
-        By distributing loans instead of grants and targeting for-profit projects, this program can operate without
-        depending on an ongoing fundraising effort. In the future, we hope to grow it to also distribute grants to
-        support noncommercial art endeavors, such as public murals, free performances, and free art classes.
-    </p>
-</article>
+<div id="about">
+    <article>
+        <h2>
+            What is the Vore Arts Fund?
+        </h2>
+        <p>
+            The Vore Arts Fund is a 501(c)(3) not-for-profit company incorporated in 2020 to financially support the arts
+            and entertainment community of Muncie, Indiana. It distributes no-contract, no-interest loans to cover the
+            up-front costs of producing for-profit art, music, performances, and art education.
+        </p>
+        <p>
+            By absorbing the financial risk inherent in arts-related projects with up-front costs, this program makes it
+            easier for artists, educators, performers, and promoters to improve the quality-of-place of our community.
+            By distributing loans instead of grants and targeting for-profit projects, this program can operate without
+            depending on an ongoing fundraising effort. In the future, we hope to grow it to also distribute grants to
+            support noncommercial art endeavors, such as public murals, free performances, and free art classes.
+        </p>
+    </article>
 
-<article class="about">
-    <h2>
-        Who's eligible?
-    </h2>
-    <p>
-        To apply for funding, you must be a resident of Muncie, Indiana, at least 18, and seeking support for the
-        up-front costs of an arts-related project that is expected to generate enough money to pay back those costs.
-    </p>
-    <p>
-        For more details about who qualifies, refer to the <a href="/terms#eligibility">eligibility requirements</a>
-        section of our Terms of Service page.
-    </p>
-</article>
+    <article>
+        <h2>
+            Who's eligible?
+        </h2>
+        <p>
+            To apply for funding, you must be a resident of Muncie, Indiana, at least 18, and seeking support for the
+            up-front costs of an arts-related project that is expected to generate enough money to pay back those costs.
+        </p>
+        <p>
+            For more details about who qualifies, refer to the <a href="/terms#eligibility">eligibility requirements</a>
+            section of our Terms of Service page.
+        </p>
+    </article>
 
-<article class="about">
-    <h2>
-        How are funding decisions made?
-    </h2>
-    <p>
-        A review committee reviews all applications to make sure they meet our eligibility requirements, and any
-        accepted applications are presented to the public to vote on, using ranked-choice voting. Once the voting period
-        is over, the money budgeted for that funding cycle gets distributed to the highest-ranked application, followed
-        by the second-highest, and so on until that cycle's budget is spent. In other words,
-        <strong>the public determines our funding priorities</strong>.
-    </p>
-</article>
+    <article>
+        <h2>
+            How are funding decisions made?
+        </h2>
+        <p>
+            A review committee reviews all applications to make sure they meet our eligibility requirements, and any
+            accepted applications are presented to the public to vote on, using ranked-choice voting. Once the voting period
+            is over, the money budgeted for that funding cycle gets distributed to the highest-ranked application, followed
+            by the second-highest, and so on until that cycle's budget is spent. In other words,
+            <strong>the public determines our funding priorities</strong>.
+        </p>
+    </article>
 
-<article class="about">
-    <h2>
-        What are the loan repayment terms?
-    </h2>
-    <p>
-        No interest is charged for any loans, but recipients are encouraged to add "voluntary interest" to their
-        repayment (kind of like adding gratuity) to help our program continue to grow. Until your loan is
-    </p>
-    <p>
-        For more details about loan terms, refer to the <a href="/terms#loan-terms">loan terms</a> section of our
-        Terms of Service page.
-    </p>
-</article>
+    <article>
+        <h2>
+            What are the loan repayment terms?
+        </h2>
+        <p>
+            No interest is charged for any loans, but recipients are encouraged to add "voluntary interest" to their
+            repayment (kind of like adding gratuity) to help our program continue to grow. Until your loan is
+        </p>
+        <p>
+            For more details about loan terms, refer to the <a href="/terms#loan-terms">loan terms</a> section of our
+            Terms of Service page.
+        </p>
+    </article>
 
-What if my funded project fails?
+    <article>
+        <h2>
+            What if my funded project fails?
+        </h2>
+        <p>
+            In the event a funded project fails to generate enough money to repay a loan, that loan can be unconditionally
+            forgiven. The Vore Arts Fund accepts the risk of your project potentially failing, and that helps you, the
+            artist, to focus on your work without worrying about whether it will bankrupt you.
+        </p>
+    </article>
 
-In the event a funded project
-fails to generate enough money to repay a loan, that loan can be unconditionally forgiven. The Vore Arts Fund
-accepts the risk of your project potentially failing, and that helps you, the artist, to focus on your work
-without worrying about whether it will bankrupt you.
-
-<div class="row">
-    <div class="col-sm-6">
-        <section class="credits">
-            <h2>
-                Board of Directors
-            </h2>
-            <dl>
-                <div>
-                    <dt>
-                        President
-                    </dt>
-                    <dd>
-                        Graham Watson
-                    </dd>
-                </div>
-                <div>
-                    <dt>
-                        Secretary
-                    </dt>
-                    <dd>
-                        Natalie Phillips
-                    </dd>
-                </div>
-                <div>
-                    <dt>
-                        Treasurer
-                    </dt>
-                    <dd>
-                        Janet Shindler
-                    </dd>
-                </div>
-            </dl>
-        </section>
-    </div>
-    <div class="col-sm-6">
-        <section class="credits">
-            <h2>
-                Website
-            </h2>
-            <dl>
-                <div>
-                    <dt>
-                        Lead Web Developer
-                    </dt>
-                    <dd>
-                        Graham Watson
-                    </dd>
-                </div>
-                <div>
-                    <dt>
-                        Web Developers
-                    </dt>
-                    <dd>
-                        Dakota Savage
-                        <br />
-                        Alec Schimmel
-                        <br />
-                        Madison Turley
-                        <br />
-                        Sean Wolfe
-                    </dd>
-                </div>
-            </dl>
-        </section>
+    <div class="row">
+        <div class="col-sm-6">
+            <section class="credits">
+                <h2>
+                    Board of Directors
+                </h2>
+                <dl>
+                    <div>
+                        <dt>
+                            President
+                        </dt>
+                        <dd>
+                            Graham Watson
+                        </dd>
+                    </div>
+                    <div>
+                        <dt>
+                            Secretary
+                        </dt>
+                        <dd>
+                            Natalie Phillips
+                        </dd>
+                    </div>
+                    <div>
+                        <dt>
+                            Treasurer
+                        </dt>
+                        <dd>
+                            Janet Shindler
+                        </dd>
+                    </div>
+                </dl>
+            </section>
+        </div>
+        <div class="col-sm-6">
+            <section class="credits">
+                <h2>
+                    Website
+                </h2>
+                <dl>
+                    <div>
+                        <dt>
+                            Lead Web Developer
+                        </dt>
+                        <dd>
+                            Graham Watson
+                        </dd>
+                    </div>
+                    <div>
+                        <dt>
+                            Web Developers
+                        </dt>
+                        <dd>
+                            Dakota Savage
+                            <br />
+                            Alec Schimmel
+                            <br />
+                            Madison Turley
+                            <br />
+                            Sean Wolfe
+                        </dd>
+                    </div>
+                </dl>
+            </section>
+        </div>
     </div>
 </div>
