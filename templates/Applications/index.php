12ba083818d4960f082bfe7d9185f4bdcac9befc
Graham Watson <graham@phantomwatson.com>
2023-02-12 23:14:26 -0500

    Change layout of my-applications

--- templates/Applications/index.php
+++ templates/Applications/index.php
@@ -32,103 +32,83 @@ $reportWhen = [
 </p>
 
 <?php if ($applications->count()): ?>
-    <?php foreach ($applications as $application): ?>
-        <section class="my-applications__application bordered-section">
-            <h2>
-                <?= $application->title ?>
-            </h2>
-            <table class="table">
-                <tbody>
-                    <tr>
-                        <th>Created:</th>
-                        <td>
-                            <?= $application->created->format('F j, Y') ?>
-                            <br />
-                            <?= $application->funding_cycle->name ?> funding cycle
-                        </td>
-                        <td>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Project</th>
+                <th>Created</th>
+                <th>Status</th>
+                <th>Reports</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($applications as $application): ?>
+                <tr>
+                    <td>
+                        <?= $application->title ?>
+                    </td>
+                    <td>
+                        <?= $application->created->format('F j, Y') ?>
+                        <br />
+                        <?= $application->funding_cycle->name ?> funding cycle
+                    </td>
+                    <td>
+                        <?= $application->status_name ?>
+                    </td>
+                    <td>
+                        <?= count($application->reports) ?>
+                    </td>
+                    <td>
+                        <?= $this->Html->link(
+                            'View',
+                            [
+                                'controller' => 'Applications',
+                                'action' => 'viewMy',
+                                'id' => $application->id,
+                                '?' => ['back' => Router::url()],
+                            ],
+                            ['title' => 'View application', 'class' => 'btn btn-secondary']
+                        ) ?>
+                        <?php if (in_array($application->status_id, $updateWhen)): ?>
                             <?= $this->Html->link(
-                                'View',
+                                'Update / Submit',
                                 [
                                     'controller' => 'Applications',
-                                    'action' => 'viewMy',
+                                    'action' => 'edit',
                                     'id' => $application->id,
-                                    '?' => ['back' => Router::url()],
                                 ],
-                                ['title' => 'View application', 'class' => 'btn btn-secondary']
+                                ['class' => 'btn btn-secondary']
                             ) ?>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>Status:</th>
-                        <td>
-                            <?= $application->status_name ?>
-                        </td>
-                        <td>
-                            <?php if (in_array($application->status_id, $updateWhen)): ?>
-                                <?= $this->Html->link(
-                                    'Update / Submit',
-                                    [
-                                        'controller' => 'Applications',
-                                        'action' => 'edit',
-                                        'id' => $application->id,
-                                    ],
-                                    ['class' => 'btn btn-secondary']
-                                ) ?>
-                            <?php endif; ?>
-                            <?php if (in_array($application->status_id, $withdrawWhen)): ?>
-                                <?= $this->Html->link(
-                                    'Withdraw',
-                                    [
-                                        'controller' => 'Applications',
-                                        'action' => 'withdraw',
-                                        'id' => $application->id,
-                                    ],
-                                    ['class' => 'btn btn-secondary']
-                                ) ?>
-                            <?php endif; ?>
-                        </td>
-                    </tr>
-                    <?php if (in_array($application->status_id, $reportWhen)): ?>
-                        <tr>
-                            <th>Reports:</th>
-                            <td>
-                                <?php $count = count($application->reports); ?>
-                                <?php if ($count): ?>
-                                    <?= $this->Html->link(
-                                        sprintf(
-                                            '%s %s submitted',
-                                            $count,
-                                            __n('report', 'reports', $count)
-                                        ),
-                                        [
-                                            'controller' => 'Reports',
-                                            'action' => 'application',
-                                            $application->id,
-                                            '?' => ['back' => Router::url()],
-                                        ]
-                                    ) ?>
-                                <?php else: ?>
-                                    No reports submitted
-                                <?php endif; ?>
-                            </td>
-                            <td>
-                                <?= $this->Html->link(
-                                    'Submit report',
-                                    [
-                                        'controller' => 'Reports',
-                                        'action' => 'submit',
-                                        $application->id,
-                                    ],
-                                    ['class' => 'btn btn-secondary']
-                                ) ?>
-                            </td>
-                        </tr>
-                    <?php endif; ?>
-                </tbody>
-            </table>
-        </section>
-    <?php endforeach; ?>
+                        <?php endif; ?>
+                        <?php if (in_array($application->status_id, $withdrawWhen)): ?>
+                            <?= $this->Html->link(
+                                'Withdraw',
+                                [
+                                    'controller' => 'Applications',
+                                    'action' => 'withdraw',
+                                    'id' => $application->id,
+                                ],
+                                ['class' => 'btn btn-secondary']
+                            ) ?>
+                        <?php endif; ?>
+                        <?php if (in_array($application->status_id, $reportWhen)): ?>
+                            <?= $this->Html->link(
+                                'Submit report',
+                                [
+                                    'controller' => 'Reports',
+                                    'action' => 'submit',
+                                    $application->id,
+                                ],
+                                ['class' => 'btn btn-secondary']
+                            ) ?>
+                        <?php endif; ?>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+
 <?php else: ?>
     <p class="alert alert-info">
         You have not yet submitted any applications for funding.
