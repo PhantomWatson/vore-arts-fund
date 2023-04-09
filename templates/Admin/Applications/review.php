fcba7a0c2eae11a86efd8ec7931f5bb8cb60322a
Graham Watson <graham@phantomwatson.com>
2023-04-09 02:42:04 -0400

    Tweak review page design

--- templates/Admin/Applications/review.php
+++ templates/Admin/Applications/review.php
@@ -35,58 +35,60 @@
         </table>
     </div>
     <div class="col-md-6 card" id="review-action-column">
-        <section class="card-body">
-            <h3>
-                Status: <?= $application->status_name ?>
-            </h3>
-            <?php if ($statusOptions): ?>
-                <?= $this->Form->create($application) ?>
-                <label class="control-label" for="change-status">
-                    Change status to
-                </label>
-                <select name="status_id" class="form-select" id="change-status">
-                    <?php foreach ($statusOptions as $statusId => $statusName): ?>
-                        <option value="<?= $statusId ?>">
-                            <?= $statusName ?>
-                        </option>
-                    <?php endforeach; ?>
-                </select>
-                <?= $this->Form->submit('Update status', ['class' => 'btn btn-primary']) ?>
-                <?= $this->Form->end() ?>
-            <?php endif; ?>
-        </section>
+        <div class="card-body">
+            <section>
+                <h3>
+                    Status: <?= $application->status_name ?>
+                </h3>
+                <?php if ($statusOptions): ?>
+                    <?= $this->Form->create($application) ?>
+                    <label class="control-label" for="change-status">
+                        Change status to
+                    </label>
+                    <select name="status_id" class="form-select" id="change-status">
+                        <?php foreach ($statusOptions as $statusId => $statusName): ?>
+                            <option value="<?= $statusId ?>">
+                                <?= $statusName ?>
+                            </option>
+                        <?php endforeach; ?>
+                    </select>
+                    <?= $this->Form->submit('Update status', ['class' => 'btn btn-primary']) ?>
+                    <?= $this->Form->end() ?>
+                <?php endif; ?>
+            </section>
 
-        <section class="card-body">
-            <h3>
-                Notes
-            </h3>
-            <?= $this->Form->create($newNote) ?>
-            <?= $this->Form->control(
-                'body',
-                [
-                    'type' => 'textarea',
-                    'label' => false
-                ]
-            ) ?>
-            <?= $this->Form->submit('Add note', ['class' => 'btn btn-primary']) ?>
-            <?= $this->Form->end() ?>
-            <?php if (!$notes->isEmpty()): ?>
-                <div id="review-notes">
-                    <?php foreach ($notes as $note): ?>
-                        <section>
-                            <h4>
-                                <?= $note->user->name ?>
-                            </h4>
-                            <p>
-                                <?= nl2br($note->body) ?>
-                            </p>
-                            <p class="date">
-                                <?= $note->created->format('F j, Y') ?>
-                            </p>
-                        </section>
-                    <?php endforeach; ?>
-                </div>
-            <?php endif; ?>
-        </section>
+            <section>
+                <h3>
+                    Notes
+                </h3>
+                <?= $this->Form->create($newNote) ?>
+                <?= $this->Form->control(
+                    'body',
+                    [
+                        'type' => 'textarea',
+                        'label' => false
+                    ]
+                ) ?>
+                <?= $this->Form->submit('Add note', ['class' => 'btn btn-primary']) ?>
+                <?= $this->Form->end() ?>
+                <?php if (!$notes->isEmpty()): ?>
+                    <div id="review-notes">
+                        <?php foreach ($notes as $note): ?>
+                            <section>
+                                <h4>
+                                    <?= $note->user->name ?>
+                                </h4>
+                                <p>
+                                    <?= nl2br($note->body) ?>
+                                </p>
+                                <p class="date">
+                                    <?= $note->created->format('F j, Y') ?>
+                                </p>
+                            </section>
+                        <?php endforeach; ?>
+                    </div>
+                <?php endif; ?>
+            </section>
+        </div>
     </div>
 </div>
