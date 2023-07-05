b290899e8f73b58de2834c0010f3d7f26b87f6ee
Graham Watson <graham@phantomwatson.com>
2023-07-04 21:49:13 -0400

    Add text message consent note

--- templates/Users/register.php
+++ templates/Users/register.php
@@ -28,7 +28,9 @@
                 'placeholder' => 'Cell number with area code, e.g. 765-123-4567'
             ]) ?>
             <p class="footnote">
-                We only use this to verify your account and prevent multi-account abuse
+                We only use this to verify your account and prevent multi-account abuse. Submitting your phone number
+                is optional, but required if you wish to vote on applications. By submitting your phone number, you
+                consent to receive a verification code via text message.
             </p>
         </div>
     </fieldset>
