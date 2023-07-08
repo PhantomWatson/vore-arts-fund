df76be931aeb197a9ea741143fb3abee8643687a
Graham Watson <graham@phantomwatson.com>
2023-07-08 18:35:06 -0400

    Update loan terms and list design

--- templates/element/loan_terms.php
+++ templates/element/loan_terms.php
@@ -1,27 +1,67 @@
-<ul>
+<ol>
     <li>
-        No interest will be charged for any loans issued by the Vore Arts Fund (hereafter, "loans").
+        <strong>
+            Payments
+        </strong>
+        <ol type="a">
+            <li>
+                No interest will be charged for any loans issued by the Vore Arts Fund (hereafter, "loans").
+            </li>
+            <li>
+                Loans can be repaid partially or in full at any time, and there is no deadline for full repayment.
+            </li>
+            <li>
+                Loans can be repaid via check, made out to Vore Arts Fund, Inc. and mailed to PO Box 1604, Muncie,
+                IN, 47308.
+            </li>
+        </ol>
     </li>
     <li>
-        Loans can be repaid partially or in full at any time, and there is no deadline for full repayment.
+        <strong>
+            Risks
+        </strong>
+        <ol type="a">
+            <li>
+                There are no circumstances in which a recipient's loan will be considered to be in default.
+            </li>
+            <li>
+                Information about this loan and its repayment status may be released publicly by the Vore Arts Fund but
+                will not be reported to any credit bureaus.
+            </li>
+        </ol>
     </li>
     <li>
-        Loans have no default terms.
+        <strong>
+            Recipient's responsibilities
+        </strong>
+        <ol type="a">
+            <li>
+                Reports about the status of this project must be submitted to the Vore Arts Fund at least annually and
+                upon its completion. A form is provided on this website for submitting these reports, and loan
+                recipients will be emailed reminders when reports are due.
+            </li>
+            <li>
+                If loans are fully or partially forgiven, the forgiven portion must be reported to the IRS as
+                taxable income if it totals $600 or more.
+            </li>
+        </ol>
     </li>
     <li>
-        Loans can be repaid via check, made out to Vore Arts Fund, Inc. and mailed to PO Box 1604, Muncie,
-        IN, 47308.
+        <strong>
+            Other
+        </strong>
+        <ol type="a">
+            <li>
+                By applying for a loan, you grant the Vore Arts Fund the right to publicly release the provided
+                information and media to the public, which it may do for purposes such as transparency and promotion.
+            </li>
+            <li>
+                The Vore Arts Fund does not claim project ownership nor a business relationship with the applicant by
+                awarding a loan. Loan recipients retain all ownership of and copyright to their intellectual property.
+            </li>
+        </ol>
     </li>
-    <li>
-        If loans are fully or partially forgiven, the forgiven portion must be reported to the IRS as
-        taxable income if it totals $600 or more.
-    </li>
-    <li>
-        Reports about the status of this project must be submitted at least annually and upon its completion.
-        A form is provided on this website for submitting these reports.
-    </li>
-    <li>
-        The Vore Arts Fund does not claim project ownership nor a business relationship with the applicant by awarding
-        a loan. You retain all ownership and copyright to your intellectual property.
-    </li>
-</ul>
+</ol>
+<p>
+    Updated July 8, 2023
+</p>
