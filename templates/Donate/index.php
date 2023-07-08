ac0e0fd555dceafacfb6ea9d32987e8bb4caba58
Graham Watson <graham@phantomwatson.com>
2023-07-08 18:07:14 -0400

    Add Patreon link; redesign donate page

--- templates/Donate/index.php
+++ templates/Donate/index.php
@@ -15,14 +15,51 @@ $formAction = \Cake\Routing\Router::url([
     and request a donation confirmation letter for your records.
 </p>
 
-<?= $this->Form->create(null, ['id' => 'donate-index', 'method' => 'post', 'action' => $formAction]) ?>
-    <div class="input-group mb-3">
-        <span class="input-group-text">$</span>
-        <input name="amount" type="number" class="form-control" aria-label="Amount to donate" min="1" id="donation-amount" required="required">
-        <span class="input-group-text">.00</span>
+<div class="row row--donate">
+    <div class="col">
+        <section class="card card--donate">
+            <div class="card-body">
+                <h1 class="card-title">
+                    Make a one-time donation
+                </h1>
+                <?= $this->Form->create(null, ['id' => 'donate-index', 'method' => 'post', 'action' => $formAction]) ?>
+                <div class="row">
+                    <div class="col">
+                        <p class="input-group">
+                            <span class="input-group-text">$</span>
+                            <input name="amount" type="number" class="form-control" aria-label="Amount to donate" min="1" id="donation-amount" required="required">
+                            <span class="input-group-text">.00</span>
+                        </p>
+                    </div>
+                    <div class="col">
+                        <p>
+                            <?= $this->Form->submit(__('Continue to payment information'), ['class' => 'btn btn-primary']) ?>
+                        </p>
+                    </div>
+                </div>
+                <?= $this->Form->end() ?>
+            </div>
+        </section>
     </div>
-<?= $this->Form->submit(__('Continue to payment information'), ['class' => 'btn btn-primary']) ?>
-<?= $this->Form->end() ?>
+    <div class="col">
+        <section class="card card--donate">
+            <div class="card-body">
+                <h1 class="card-title">
+                    Support with monthly donations
+                </h1>
+                <p class="card-text">
+                    Any support is appreciated, but the best way to help us keep this program growing is to become a supporter
+                    through <strong>Patreon</strong> and contribute monthly donations.
+                </p>
+                <p class="card-text">
+                    <a href="https://www.patreon.com/VoreArtsFund" class="btn btn-primary">
+                        Sign up for recurring donations
+                    </a>
+                </p>
+            </div>
+        </section>
+    </div>
+</div>
 
 <script>
     const form = document.getElementById('donate-index');
