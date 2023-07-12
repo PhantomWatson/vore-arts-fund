36e6fc2ae6d1637fe4dd3cfd94f1f748699c297d
Graham Watson <graham@phantomwatson.com>
2023-07-12 00:49:47 -0400

    Handle breadcrumb counts of 0 and 1

--- templates/element/breadcrumbs.php
+++ templates/element/breadcrumbs.php
@@ -15,20 +15,22 @@ if ($here == '/') {
 }
 
 // Avoid redundant "Home > Controller / Group Title > Page Title" breadcrumbs, like "Home > Admin > Admin"
-if (end($breadcrumbs)[0] == $currentBreadcrumb) {
+if ($breadcrumbs && end($breadcrumbs)[0] == $currentBreadcrumb) {
     array_pop($breadcrumbs);
 }
 ?>
 
-<nav aria-label="breadcrumb">
-    <ol class="breadcrumb">
-        <?php foreach ($breadcrumbs as $breadcrumb): ?>
-            <li class="breadcrumb-item">
-                <?= $this->Html->link($breadcrumb[0], $breadcrumb[1]) ?>
+<?php if (count($breadcrumbs) > 1): ?>
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <?php foreach ($breadcrumbs as $breadcrumb): ?>
+                <li class="breadcrumb-item">
+                    <?= $this->Html->link($breadcrumb[0], $breadcrumb[1]) ?>
+                </li>
+            <?php endforeach; ?>
+            <li class="breadcrumb-item active" aria-current="page">
+                <?= $currentBreadcrumb ?>
             </li>
-        <?php endforeach; ?>
-        <li class="breadcrumb-item active" aria-current="page">
-            <?= $currentBreadcrumb ?>
-        </li>
-    </ol>
-</nav>
+        </ol>
+    </nav>
+<?php endif; ?>
