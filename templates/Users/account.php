b93b986d00ec0461d08fe9004fdb5ecc770992f5
Graham Watson <graham@phantomwatson.com>
2023-09-24 02:50:46 -0400

    Add verify link to account index page

--- templates/Users/account.php
+++ templates/Users/account.php
@@ -9,6 +9,21 @@
 use App\Model\Entity\Application;
 ?>
 
+<?php if ($user->is_verified): ?>
+    <p class="alert alert-success">
+        Phone number verified
+    </p>
+<?php else: ?>
+    <?= $this->Html->link(
+        'Verify phone number',
+        [
+            'controller' => 'Users',
+            'action' => 'verify',
+        ],
+        ['class' => 'btn btn-secondary']
+    ) ?>
+<?php endif; ?>
+
 <?= $this->Html->link(
     'Change Account Info',
     [
