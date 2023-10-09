bb595eda696107b2dc187e505a65fa72bd11a94c
Graham Watson <graham@phantomwatson.com>
2023-10-08 23:59:49 -0400

    Disable hostname validation in reCAPTCHA
    Wasn't working, for some reason.

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -101,8 +101,7 @@ class UsersController extends AppController
     {
         $captchaResponse = $_POST['g-recaptcha-response'];
         $recaptcha = new \ReCaptcha\ReCaptcha(Configure::read('recaptcha.secretKey'));
-        $resp = $recaptcha->setExpectedHostname(gethostname() ?: 'voreartsfund.org')
-            ->verify($captchaResponse, $this->getRequest()->clientIp());
+        $resp = $recaptcha->verify($captchaResponse, $this->getRequest()->clientIp());
         if ($resp->isSuccess()) {
             return true;
         }
