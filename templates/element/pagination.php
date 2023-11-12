f91afd176b1fe56e96a311cb6b74d39aa339a7c0
Graham Watson <graham@phantomwatson.com>
2023-11-11 21:42:48 -0500

    Make display of pagination element conditional

--- templates/element/pagination.php
+++ templates/element/pagination.php
@@ -1,7 +1,14 @@
-<div class="pagination">
-    <ul class="pagination">
-        <?= $this->Paginator->prev() ?>
-        <?= $this->Paginator->numbers() ?>
-        <?= $this->Paginator->next() ?>
-    </ul>
-</div>
+<?php
+/**
+ * @var \App\View\AppView $this
+ */
+?>
+<?php if ($this->Paginator->hasPrev() || $this->Paginator->hasNext()): ?>
+    <div class="pagination">
+        <ul class="pagination">
+            <?= $this->Paginator->prev() ?>
+            <?= $this->Paginator->numbers() ?>
+            <?= $this->Paginator->next() ?>
+        </ul>
+    </div>
+<?php endif; ?>
