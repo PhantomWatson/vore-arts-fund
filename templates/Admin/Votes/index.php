95a81b3973d3f8cce86490158eccaf3bd30970af
Graham Watson <graham@phantomwatson.com>
2024-01-07 16:49:30 -0500

    Mark projects as having been awarded or not

--- templates/Admin/Votes/index.php
+++ templates/Admin/Votes/index.php
@@ -1,10 +1,13 @@
 <?php
 /**
  * @var \App\View\AppView $this
- * @var \App\Model\Entity\Project[] $projects
+ * @var Project[] $projects
  * @var \App\Model\Entity\FundingCycle|null $fundingCycle
  * @var \App\Model\Entity\FundingCycle[] $fundingCycles
  */
+
+use App\Model\Entity\Project;
+
 $fundingAvailable = $fundingCycle->funding_available;
 ?>
 
@@ -44,42 +47,55 @@ $fundingAvailable = $fundingCycle->funding_available;
                     Project
                 </th>
                 <th>
-                    Total score
-                </th>
-                <th>
-                    Funding requested
-                </th>
-                <th>
-                    To Award
+                    Award
                 </th>
             </tr>
         </thead>
         <tbody>
             <?php foreach ($projects as $i => $project): ?>
                 <?php
+                    $fundable = false;
+                    $isPartial = false;
                     if ($project->voting_score == null) {
                         $toAward = 'N/A';
                     } elseif ($project->amount_requested <= $fundingAvailable) {
                         $toAward = '$' . number_format($project->amount_requested);
                         $fundingAvailable -= $project->amount_requested;
+                        $fundable = true;
                     } elseif ($fundingAvailable && $project->accept_partial_payout) {
-                        $toAward = '$' . number_format($fundingAvailable) . ' (partial payout)';
+                        $toAward = '$' . number_format($fundingAvailable);
                         $fundingAvailable = 0;
+                        $fundable = true;
+                        $isPartial;
                     } else {
                         $toAward = 'Unable to fund';
+                    }
+                    if ($fundable) {
+                        if ($isPartial) {
+                            $toAward .= ' (partial payout)';
+                        }
+                        if ($project->status_id == Project::STATUS_AWARDED) {
+                            $toAward = '<span class="voting-results__award voting-results__award--awarded">'
+                                . '<i class="fa-solid fa-circle-check"></i> '
+                                . $toAward . ' awarded</span>';
+                        } else {
+                            $toAward = '<span class="voting-results__award voting-results__award--to-award">'
+                                . '<i class="fa-solid fa-circle-exclamation"></i> '
+                                . $toAward . ' to award</span>';
+                        }
                     }
                 ?>
                 <tr>
                     <td>
                         <?= $project->voting_score == null ? '' : $i + 1 ?>
+                        <br />
+                        <span class="voting-results__score">
+                            <?= $project->voting_score === null ? 'No votes' : ('Score: ' . $project->voting_score) ?>
+                        </span>
                     </td>
                     <td>
                         <?= $project->title ?>
-                    </td>
-                    <td>
-                        <?= $project->voting_score === null ? 'No votes' : $project->voting_score ?>
-                    </td>
-                    <td>
+                        <br />
                         <?= $project->amount_requested_formatted ?>
                     </td>
                     <td>
