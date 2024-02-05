ccda8ca9fc7b5937b8a07068f75d28e522d63a28
Graham Watson <graham@phantomwatson.com>
2024-02-05 02:50:19 -0500

    Avoid JS errors on non-gallery pages

--- webroot/js/image-viewer.js
+++ webroot/js/image-viewer.js
@@ -1,19 +1,21 @@
-new Viewer(
-  document.querySelector('.image-gallery'),
-  {
-    url: 'data-full',
-    toolbar: {
-      zoomIn: true,
-      zoomOut: true,
-      oneToOne: false,
-      reset: true,
-      prev: true,
-      play: false,
-      next: true,
-      rotateLeft: false,
-      rotateRight: false,
-      flipHorizontal: false,
-      flipVertical: false,
-    },
-  }
-);
+if (document.querySelector('.image-gallery')) {
+  new Viewer(
+    document.querySelector('.image-gallery'),
+    {
+      url: 'data-full',
+      toolbar: {
+        zoomIn: true,
+        zoomOut: true,
+        oneToOne: false,
+        reset: true,
+        prev: true,
+        play: false,
+        next: true,
+        rotateLeft: false,
+        rotateRight: false,
+        flipHorizontal: false,
+        flipVertical: false,
+      },
+    }
+  );
+}
