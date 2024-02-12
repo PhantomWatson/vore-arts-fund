600fec10c6372b0a8ca1865c3748d7ab15196ddd
Graham Watson <graham@phantomwatson.com>
2024-02-12 02:54:13 -0500

    Add DOMContentLoaded

--- webroot/js/image-viewer.js
+++ webroot/js/image-viewer.js
@@ -1,21 +1,23 @@
 if (document.querySelector('.image-gallery')) {
-  new Viewer(
-    document.querySelector('.image-gallery'),
-    {
-      url: 'data-full',
-      toolbar: {
-        zoomIn: true,
-        zoomOut: true,
-        oneToOne: false,
-        reset: true,
-        prev: true,
-        play: false,
-        next: true,
-        rotateLeft: false,
-        rotateRight: false,
-        flipHorizontal: false,
-        flipVertical: false,
-      },
-    }
-  );
+  window.addEventListener('DOMContentLoaded', (event) => {
+    new Viewer(
+      document.querySelector('.image-gallery'),
+      {
+        url: 'data-full',
+        toolbar: {
+          zoomIn: true,
+          zoomOut: true,
+          oneToOne: false,
+          reset: true,
+          prev: true,
+          play: false,
+          next: true,
+          rotateLeft: false,
+          rotateRight: false,
+          flipHorizontal: false,
+          flipVertical: false,
+        },
+      }
+    );
+  });
 }
