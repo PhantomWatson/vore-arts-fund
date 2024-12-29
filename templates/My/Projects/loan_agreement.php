4e3ae68d4c620201d77f7eaf59951ff551e7385e
Graham Watson <graham@phantomwatson.com>
2024-12-28 20:31:15 -0500

    Differentiate between signed and not-signed versions of agreement page

--- templates/My/Projects/loan_agreement.php
+++ templates/My/Projects/loan_agreement.php
@@ -2,13 +2,9 @@
 /**
  * @var \App\Model\Entity\Project $project
  */
-$date = $project->loan_agreement_date
-    ? $project->loan_agreement_date->setTimezone(\App\Application::LOCAL_TIMEZONE)->format('F j, Y')
-    : (new \DateTime())->format('F j, Y');
 $dueDate = $project->loan_due_date
     ? $project->loan_due_date->setTimezone(\App\Application::LOCAL_TIMEZONE)->format('F j, Y')
     : (new \DateTime(\App\Model\Entity\Project::DUE_DATE))->format('F j, Y');
-$signed = (bool)$project->loan_agreement_date;
 $amount = $project->amount_awarded_formatted;
 ?>
 
@@ -19,31 +15,30 @@ $amount = $project->amount_awarded_formatted;
 
 <?= $this->element('loan_terms', compact('amount', 'dueDate')) ?>
 
-<?= $this->Form->create($project) ?>
-
-<section class="loan-agreement">
-    <h2>
-        Signature
-    </h2>
-
+<?php if ($project->loan_agreement_date): ?>
     <p>
-        By entering the following information, you signify that you agree to these terms on behalf of the borrower and are authorized to do so.
-        <br />
-        For loans to <strong>individuals</strong>, enter the borrower's <strong>social security number</strong>
-        <br />
-        For loans to <strong>businesses</strong>, enter the borrower's <strong>federal employer identification number</strong>
+        <strong>Agreement date:</strong>
+        <?= $project->loan_agreement_date->setTimezone(\App\Application::LOCAL_TIMEZONE)->format('F j, Y') ?>
     </p>
+<?php else: ?>
+    <?= $this->Form->create($project) ?>
+    <section class="loan-agreement">
+        <h2>
+            Signature
+        </h2>
 
-    <?= $this->Form->control('tin', ['type' => 'text', 'label' => 'Tax ID Number', 'class' => 'form-control']) ?>
-    <?= $this->Form->control('tin_confirm', ['label' => 'Re-enter Tax ID Number', 'class' => 'form-control']) ?>
+        <p>
+            By entering the following information, you signify that you agree to these terms on behalf of the borrower and are authorized to do so.
+            <br />
+            For loans to <strong>individuals</strong>, enter the borrower's <strong>social security number</strong>
+            <br />
+            For loans to <strong>businesses</strong>, enter the borrower's <strong>federal employer identification number</strong>
+        </p>
 
-    <p>
-        <strong>Agreement date:</strong> <?= $date ?>
-    </p>
-</section>
-
-<input type="hidden" name="agreement" value="1" />
-
-<?= $this->Form->submit('Submit', ['class' => 'btn btn-primary']) ?>
-
-<?= $this->Form->end() ?>
+        <?= $this->Form->control('tin', ['type' => 'text', 'label' => 'Tax ID Number', 'class' => 'form-control']) ?>
+        <?= $this->Form->control('tin_confirm', ['label' => 'Re-enter Tax ID Number', 'class' => 'form-control']) ?>
+        <input type="hidden" name="agreement" value="1" />
+    </section>
+    <?= $this->Form->submit('Submit', ['class' => 'btn btn-primary']) ?>
+    <?= $this->Form->end() ?>
+<?php endif; ?>
