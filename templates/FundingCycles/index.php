89792628d8036f39b8f8f5ef08215275e1437f85
Graham Watson <graham@phantomwatson.com>
2025-01-01 22:08:01 -0500

    Stash funding cycle calendar on index page

--- templates/FundingCycles/index.php
+++ templates/FundingCycles/index.php
@@ -3,6 +3,18 @@
  * @var \App\Model\Entity\FundingCycle[] $fundingCycles
  * @var string $title
  */
+$suggestedCycles = [];
+function monthName($monthNumber) {
+    return date('M', mktime(0, 0, 0, $monthNumber, 1));
+}
+for ($n = 1; $n <= 12; $n++) {
+    $monthName = monthName($n);
+    $suggestedCycles[$monthName] = [
+        monthName($n - 2) => 'App',
+        monthName($n - 1) => 'Review + Vote',
+        monthName($n) => 'Disburse',
+    ];
+}
 ?>
 
 <?php if ($fundingCycles->count()): ?>
@@ -17,3 +29,31 @@
 <?php else: ?>
     <?= $this->element('no_funding_cycle') ?>
 <?php endif; ?>
+
+<table class="table table-bordered" id="cycles-chart">
+    <thead>
+        <tr>
+            <th>Cycle</th>
+            <?php foreach (explode(' ', 'Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec') as $month): ?>
+                <th><?= $month ?></th>
+            <?php endforeach; ?>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($suggestedCycles as $month => $cycle): ?>
+            <tr class="spring">
+                <th><?= $month ?></th>
+                <?php for ($n = 1; $n <= 12; $n++): ?>
+                    <?php $monthName = monthName($n); ?>
+                    <?php if ($cycle[$monthName] ?? false): ?>
+                        <td>
+                            <?= $cycle[$monthName] ?? '' ?>
+                        </td>
+                    <?php else: ?>
+                        <td class="blank"></td>
+                    <?php endif; ?>
+                <?php endfor; ?>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
