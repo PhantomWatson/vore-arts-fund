3d4e7fc4be4ada1ca67ba94fa9ba3df2275e01ee
Graham Watson <graham@phantomwatson.com>
2025-01-05 02:21:04 -0500

    Add instructions for replying to messages from the review committee

--- templates/email/html/message.php
+++ templates/email/html/message.php
@@ -5,12 +5,30 @@
  * @var \App\View\AppView $this
  * @var string $userName
  */
+
+use Cake\Routing\Router;
+
+$replyUrl = Router::url([
+    'prefix' => 'My',
+    'controller' => 'Projects',
+    'action' => 'messages',
+    'id' => $project->id
+]);
+
 ?>
 <p>
-    <?= $userName ?>, you've received a message from the Vore Arts Fund review committee regarding your project
+    <?= $userName ?>,
+</p>
+
+<p>
+    you've received a message from the Vore Arts Fund review committee regarding your project
     <strong><?= $project->title ?></strong>:
 </p>
 
 <blockquote>
     <?= $message ?>
 </blockquote>
+
+<p>
+    To reply to this message, please visit <a href="<?= $replyUrl ?>">the Messages page for this project</a>.
+</p>
--- templates/email/text/message.php
+++ templates/email/text/message.php
@@ -5,7 +5,20 @@
  * @var \App\View\AppView $this
  * @var string $userName
  */
+
+use Cake\Routing\Router;
+
+$replyUrl = Router::url([
+    'prefix' => 'My',
+    'controller' => 'Projects',
+    'action' => 'messages',
+    'id' => $project->id
+]);
 ?>
-<?= $userName ?>, you've received a message from the Vore Arts Fund review committee regarding your project "<?= $project->title ?>":
+<?= $userName ?>,
+
+you've received a message from the Vore Arts Fund review committee regarding your project "<?= $project->title ?>":
 
 <?= nl2br($message) ?>
+
+To reply to this message, please visit the Messages page for this project: <?= $replyUrl ?>
