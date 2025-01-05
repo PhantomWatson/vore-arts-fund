3cb21352fcff7de824d0bd6425c70c80b04eb6f6
Graham Watson <graham@phantomwatson.com>
2025-01-05 01:01:11 -0500

    Inline email CSS
    thanks, MailChimp!
    https://templates.mailchimp.com/resources/inline-css/

--- templates/layout/email/html/default.php
+++ templates/layout/email/html/default.php
@@ -14,297 +14,46 @@ $host = $this->getRequest()->host() ?: 'voreartsfund.org';
     <title>
         <?= $subject ?? '' ?>
     </title>
-    <style media="all" type="text/css">
-        /* -------------------------------------
-           GLOBAL RESETS
-           ------------------------------------- */
 
-        body {
-            font-family: Helvetica, sans-serif;
-            -webkit-font-smoothing: antialiased;
-            font-size: 16px;
-            line-height: 1.3;
-            -ms-text-size-adjust: 100%;
-            -webkit-text-size-adjust: 100%;
-        }
-
-        table {
-            border-collapse: separate;
-            mso-table-lspace: 0pt;
-            mso-table-rspace: 0pt;
-            width: 100%;
-        }
-
-        table td {
-            font-family: Helvetica, sans-serif;
-            font-size: 16px;
-            vertical-align: top;
-        }
-
-        /* -------------------------------------
-        BODY & CONTAINER
-    ------------------------------------- */
-
-        body {
-            background-color: #f4f5f6;
-            margin: 0;
-            padding: 0;
-        }
-
-        .body {
-            background-color: #f4f5f6;
-            width: 100%;
-        }
-
-        .container {
-            margin: 0 auto !important;
-            max-width: 600px;
-            padding: 0;
-            padding-top: 24px;
-            width: 600px;
-        }
-
-        .content {
-            box-sizing: border-box;
-            display: block;
-            margin: 0 auto;
-            max-width: 600px;
-            padding: 0;
-        }
-
-        /* -------------------------------------
-           HEADER, FOOTER, MAIN
-           ------------------------------------- */
-
-        .main {
-            background: #ffffff;
-            border: 1px solid #eaebed;
-            border-radius: 16px;
-            width: 100%;
-        }
-
-        .wrapper {
-            box-sizing: border-box;
-            padding: 24px;
-        }
-
-        .header {
-            background: #ffffff;
-            border: 1px solid #eaebed;
-            clear: both;
-            padding: 24px;
-            text-align: center;
-        }
-
-        .header img {
-            border: 0;
-            margin: auto;
-            max-height: 150px;
-            max-width: 75%;
-        }
-
-        .footer {
-            clear: both;
-            padding-top: 24px;
-            text-align: center;
-            width: 100%;
-        }
-
-        .footer td,
-        .footer p,
-        .footer span,
-        .footer a {
-            color: #9a9ea6;
-            font-size: 16px;
-            text-align: center;
-        }
-
-        /* -------------------------------------
-           TYPOGRAPHY
-           ------------------------------------- */
-
-        p {
-            font-family: Helvetica, sans-serif;
-            font-size: 16px;
-            font-weight: normal;
-            margin: 0;
-            margin-bottom: 16px;
-        }
-
-        a {
-            color: #0867ec;
-            text-decoration: underline;
-        }
-
-        /* -------------------------------------
-           BUTTONS
-           ------------------------------------- */
-
-        .btn {
-            box-sizing: border-box;
-            min-width: 100% !important;
-            width: 100%;
-        }
-
-        .btn > tbody > tr > td {
-            padding-bottom: 16px;
-        }
-
-        .btn table {
-            width: auto;
-        }
-
-        .btn table td {
-            background-color: #ffffff;
-            border-radius: 4px;
-            text-align: center;
-        }
-
-        .btn a {
-            background-color: #ffffff;
-            border: solid 2px #0867ec;
-            border-radius: 4px;
-            box-sizing: border-box;
-            color: #0867ec;
-            cursor: pointer;
-            display: inline-block;
-            font-size: 16px;
-            font-weight: bold;
-            margin: 0;
-            padding: 12px 24px;
-            text-decoration: none;
-            text-transform: capitalize;
-        }
-
-        .btn-primary table td {
-            background-color: #0867ec;
-        }
-
-        .btn-primary a {
-            background-color: #0867ec;
-            border-color: #0867ec;
-            color: #ffffff;
-        }
-
-        @media all {
-            .btn-primary table td:hover {
-                background-color: #ec0867 !important;
-            }
-
-            .btn-primary a:hover {
-                background-color: #ec0867 !important;
-                border-color: #ec0867 !important;
-            }
-        }
-
-        /* -------------------------------------
-           RESPONSIVE AND MOBILE FRIENDLY STYLES
-           ------------------------------------- */
-
-        @media only screen and (max-width: 640px) {
-            .main p,
-            .main td,
-            .main span {
-                font-size: 16px !important;
-            }
-
-            .wrapper {
-                padding: 8px !important;
-            }
-
-            .content {
-                padding: 0 !important;
-            }
-
-            .container {
-                padding: 0 !important;
-                padding-top: 8px !important;
-                width: 100% !important;
-            }
-
-            .main {
-                border-left-width: 0 !important;
-                border-radius: 0 !important;
-                border-right-width: 0 !important;
-            }
-
-            .btn table {
-                max-width: 100% !important;
-                width: 100% !important;
-            }
-
-            .btn a {
-                font-size: 16px !important;
-                max-width: 100% !important;
-                width: 100% !important;
-            }
-        }
-
-        /* -------------------------------------
-           PRESERVE THESE STYLES IN THE HEAD
-           ------------------------------------- */
-
-        @media all {
-            .ExternalClass {
-                width: 100%;
-            }
-
-            .ExternalClass,
-            .ExternalClass p,
-            .ExternalClass span,
-            .ExternalClass font,
-            .ExternalClass td,
-            .ExternalClass div {
-                line-height: 100%;
-            }
-
-            .apple-link a {
-                color: inherit !important;
-                font-family: inherit !important;
-                font-size: inherit !important;
-                font-weight: inherit !important;
-                line-height: inherit !important;
-                text-decoration: none !important;
-            }
-
-            #MessageViewBody a {
-                color: inherit;
-                text-decoration: none;
-                font-size: inherit;
-                font-family: inherit;
-                font-weight: inherit;
-                line-height: inherit;
-            }
-        }
-    </style>
 </head>
-<body>
-<div class="header">
-    <a href="https://<?= $host ?>">
-        <img src="https://<?= $host ?>/img/logo/logo.wordmark.v5.white.png" alt="Vore Arts Fund" title="Vore Arts Fund" />
+<body
+    style="font-family: Helvetica, sans-serif;-webkit-font-smoothing: antialiased;font-size: 16px;line-height: 1.3;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;background-color: #f4f5f6;margin: 0;padding: 0;">
+<div class="header" style="background: #ffffff;border: 1px solid #eaebed;clear: both;padding: 24px;text-align: center;">
+    <a href="https://<?= $host ?>" style="color: #0867ec;text-decoration: underline;">
+        <img src="https://<?= $host ?>/img/logo/logo.wordmark.v5.white.png" alt="Vore Arts Fund" title="Vore Arts Fund"
+             style="border: 0;margin: auto;max-height: 150px;max-width: 75%;">
     </a>
 </div>
 
-<table role="presentation" border="0" cellpadding="0" cellspacing="0" class="body">
+<table role="presentation" border="0" cellpadding="0" cellspacing="0" class="body"
+       style="border-collapse: separate;mso-table-lspace: 0pt;mso-table-rspace: 0pt;width: 100%;background-color: #f4f5f6;">
     <tr>
-        <td>&nbsp;</td>
-        <td class="container">
-            <div class="content">
+        <td style="font-family: Helvetica, sans-serif;font-size: 16px;vertical-align: top;">&nbsp;</td>
+        <td class="container"
+            style="font-family: Helvetica, sans-serif;font-size: 16px;vertical-align: top;max-width: 600px;padding: 0;padding-top: 24px;width: 600px;margin: 0 auto !important;">
+            <div class="content"
+                 style="box-sizing: border-box;display: block;margin: 0 auto;max-width: 600px;padding: 0;">
 
-                <table role="presentation" border="0" cellpadding="0" cellspacing="0" class="main">
+                <table role="presentation" border="0" cellpadding="0" cellspacing="0" class="main"
+                       style="border-collapse: separate;mso-table-lspace: 0pt;mso-table-rspace: 0pt;width: 100%;background: #ffffff;border: 1px solid #eaebed;border-radius: 16px;">
                     <tr>
-                        <td class="wrapper">
+                        <td class="wrapper"
+                            style="font-family: Helvetica, sans-serif;font-size: 16px;vertical-align: top;box-sizing: border-box;padding: 24px;">
                             <?= $this->fetch('content') ?>
                         </td>
                     </tr>
                 </table>
 
-                <div class="footer">
-                    <table role="presentation" border="0" cellpadding="0" cellspacing="0">
+                <div class="footer" style="clear: both;padding-top: 24px;text-align: center;width: 100%;">
+                    <table role="presentation" border="0" cellpadding="0" cellspacing="0"
+                           style="border-collapse: separate;mso-table-lspace: 0pt;mso-table-rspace: 0pt;width: 100%;">
                         <tr>
-                            <td class="content-block">
-                                <a href="https://<?= $host ?>">
-                                    <img src="https://<?= $host ?>/img/logo/logo.wordmark.v5.white.email.png" alt="Vore Arts Fund" title="Vore Arts Fund" />
+                            <td class="content-block"
+                                style="font-family: Helvetica, sans-serif;font-size: 16px;vertical-align: top;color: #9a9ea6;text-align: center;">
+                                <a href="https://<?= $host ?>"
+                                   style="color: #9a9ea6;text-decoration: underline;font-size: 16px;text-align: center;">
+                                    <img src="https://<?= $host ?>/img/logo/logo.wordmark.v5.white.email.png"
+                                         alt="Vore Arts Fund" title="Vore Arts Fund">
                                 </a>
                             </td>
                         </tr>
@@ -312,7 +61,7 @@ $host = $this->getRequest()->host() ?: 'voreartsfund.org';
                 </div>
             </div>
         </td>
-        <td>&nbsp;</td>
+        <td style="font-family: Helvetica, sans-serif;font-size: 16px;vertical-align: top;">&nbsp;</td>
     </tr>
 </table>
 </body>
