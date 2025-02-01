5043b2022c0189eaa447b20f8e65338c5a4ac6b3
Graham Watson <graham@phantomwatson.com>
2025-02-01 01:26:59 -0500

    Add copy about avoiding spam filtering

--- templates/MailingList/thanks.php
+++ templates/MailingList/thanks.php
@@ -1,3 +1,4 @@
 <p>
-    Stay tuned for upcoming emails with news about the Vore Arts Fund, and be sure to check your spam folder!
+    Stay tuned for upcoming emails with news about the Vore Arts Fund! Be sure to check your spam folder and save our
+    address to your contact list to make sure that you receive our messages.
 </p>
