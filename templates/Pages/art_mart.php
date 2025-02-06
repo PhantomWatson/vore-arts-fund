895ebd727e77f7a51028ae74316493667625c6e8
Graham Watson <graham@phantomwatson.com>
2025-02-06 02:48:07 -0500

    Link project titles

--- templates/Pages/art_mart.php
+++ templates/Pages/art_mart.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * @var \App\Model\Entity\User[] $users
+ * @var \App\View\AppView $this
  */
 ?>
 
@@ -31,7 +32,10 @@
                         </strong>
                     </td>
                     <td>
-                        <?= $user->projects[0]->title ?>
+                        <?= $this->Html->link(
+                            $user->projects[0]->title,
+                            ['prefix' => false, 'controller' => 'Projects', 'action' => 'view', 'id' => $user->projects[0]->id]
+                        ) ?>
                     </td>
                     <td>
                         <?= $user->projects[0]->amount_awarded_formatted ?>
@@ -43,13 +47,16 @@
                 <?php foreach (array_slice($user->projects, 1) as $project): ?>
                     <tr>
                         <td>
-                            <?= $user->projects[0]->title ?>
+                            <?= $this->Html->link(
+                                $project->title,
+                                ['prefix' => false, 'controller' => 'Projects', 'action' => 'view', 'id' => $user->projects[0]->id]
+                            ) ?>
                         </td>
                         <td>
-                            <?= $user->projects[0]->amount_awarded_formatted ?>
+                            <?= $project->amount_awarded_formatted ?>
                         </td>
                         <td>
-                            <?= $user->projects[0]->loan_agreement_date_local->format('F j, Y') ?>
+                            <?= $project->loan_agreement_date_local->format('F j, Y') ?>
                         </td>
                     </tr>
                 <?php endforeach; ?>
