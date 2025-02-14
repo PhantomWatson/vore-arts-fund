cf4cafec1ae42a417449214d0c27af1b99454225
Graham Watson <graham@phantomwatson.com>
2025-02-14 00:01:53 -0500

    Remove debug code

--- src/Controller/ProjectsController.php
+++ src/Controller/ProjectsController.php
@@ -80,7 +80,7 @@ class ProjectsController extends AppController
         // Check if applications can be accepted
         /** @var FundingCycle $fundingCycle */
         $fundingCycle = $this->FundingCycles->find('current')->first();
-        if (true || is_null($fundingCycle)) {
+        if (is_null($fundingCycle)) {
             $nextFundingCycle = $this->FundingCycles->find('nextApplying')->first();
             $this->set(compact('nextFundingCycle'));
             $this->viewBuilder()->setTemplate('applications_not_being_accepted');
