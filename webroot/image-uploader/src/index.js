495b549ff74785a8d1479ac7ff3ad8c83e173d29
Graham Watson <graham@phantomwatson.com>
2025-02-16 20:20:17 -0500

    Fix how missing preloadImages is handled

--- webroot/image-uploader/src/index.js
+++ webroot/image-uploader/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 
 const rootElement = document.getElementById('image-uploader-root');
 const root = ReactDOM.createRoot(rootElement);
-const images = window?.preloadImages;
+const images = window.preloadImages ?? [];
 
 root.render(
   <React.StrictMode>
