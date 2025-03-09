94b465e200c735f3f5e72a998dd989b355a287ce
Graham Watson <graham@phantomwatson.com>
2025-03-08 22:31:41 -0500

    Add conditional guidance for how to fill out each field

--- webroot/transaction-form/src/App.jsx
+++ webroot/transaction-form/src/App.jsx
@@ -65,6 +65,56 @@ const showAmountNet = (transactionType) => {
   }
 };
 
+// Guidance text
+function guidanceAmountGross(type) {
+  switch (+type) {
+    case TYPE_DONATION:
+    case TYPE_LOAN_REPAYMENT:
+      return 'The total amount paid, including processing fees';
+    case TYPE_CANCELED_CHECK:
+      return 'The value of the check';
+    case TYPE_LOAN:
+      return 'The amount of the loan';
+    default:
+      return 'The amount paid or received';
+  }
+}
+function guidanceProject(type) {
+  switch (+type) {
+    case TYPE_LOAN:
+    case TYPE_LOAN_REPAYMENT:
+      return 'The project that this loan supported';
+    case TYPE_CANCELED_CHECK:
+      return 'The project that this loan was intended for';
+    default:
+      return '(Optional) The project that this transaction is associated with';
+  }
+}
+function guidanceName(type) {
+  switch (+type) {
+    case TYPE_DONATION:
+    case TYPE_LOAN_REPAYMENT:
+      return 'The person paying us (leave blank for anonymous)';
+    case TYPE_CANCELED_CHECK:
+    case TYPE_LOAN:
+      return 'The person the check was payable to';
+    default:
+      return 'The person or business that money was paid to or received from';
+  }
+}
+function guidanceMetadata(type) {
+  switch (+type) {
+    case TYPE_DONATION:
+    case TYPE_LOAN_REPAYMENT:
+      return 'This is for any other information about this transaction that\'s important to remember.';
+    case TYPE_LOAN:
+      return 'Include the check number here';
+    case TYPE_CANCELED_CHECK:
+      return 'Include the check number here, as well as (optionally) the reason why the check was canceled';
+    default:
+      return 'This is for any other information about this transaction that\'s important to remember. For any checks written, the check number should be recorded here.';
+  }
+}
 
 const App = () => {
   // State
@@ -173,7 +223,7 @@ const App = () => {
                     </div>
                   </div>
                   <div className="col-sm-6">
-                    <p>The person or business that money was paid to or received from</p>
+                    <p>{guidanceName(values.type)}</p>
                   </div>
                 </div>
 
@@ -197,7 +247,9 @@ const App = () => {
                     </div>
                   </div>
                   <div className="col-sm-6">
-                    <p>The amount paid or received</p>
+                    <p>
+                      {guidanceAmountGross(values.type)}
+                    </p>
                   </div>
                 </div>
 
@@ -221,7 +273,7 @@ const App = () => {
                         </div>
                       </div>
                       <div className="col-sm-6">
-                        <p>The gross amount, minus any processing fees</p>
+                        <p>The total amount received (gross amount minus any processing fees)</p>
                       </div>
                     </div>
                   </>
@@ -255,7 +307,9 @@ const App = () => {
                         </div>
                       </div>
                       <div className="col-sm-6">
-                        <p>(Optional) The project that this transaction is associated with</p>
+                        <p>
+                          {guidanceProject(values.type)}
+                        </p>
                       </div>
                     </div>
                   </>
@@ -272,8 +326,7 @@ const App = () => {
                   </div>
                   <div className="col-sm-6">
                     <p>
-                      This is for any other information about this transaction that's important to remember.
-                      For any checks written, the check number should be recorded here.
+                      {guidanceMetadata(values.type)}
                     </p>
                   </div>
                 </div>
