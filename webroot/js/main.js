66f31becde207ab103e6a753983d101220589ba4
Graham Watson <graham@phantomwatson.com>
2025-03-09 00:30:24 -0500

    Prevent multiple loading indicators from being added

--- webroot/js/main.js
+++ webroot/js/main.js
@@ -6,10 +6,14 @@ function preventMultipleSubmit(formQuery) {
   }
   form.addEventListener('submit', function () {
     const button = this.querySelector('button[type="submit"]');
-    const loadingIndicator = document.createElement('i')
-    loadingIndicator.className = 'fa-solid fa-spinner fa-spin-pulse prevent-multiple-submit__loading-indicator';
-    loadingIndicator.title = 'Loading';
-    button.appendChild(loadingIndicator);
+    const className = 'prevent-multiple-submit__loading-indicator';
+    const existingLoadingIndicator = button.querySelector(className);
+    if (!existingLoadingIndicator) {
+      const loadingIndicator = document.createElement('i')
+      loadingIndicator.className = 'fa-solid fa-spinner fa-spin-pulse ' + className;
+      loadingIndicator.title = 'Loading';
+      button.appendChild(loadingIndicator);
+    }
     button.setAttribute('disabled', 'disabled');
   }, false);
 }
