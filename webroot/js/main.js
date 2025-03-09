ef38cc3405e62ea083cdae5155c679e7be6e5c76
Graham Watson <graham@phantomwatson.com>
2025-03-08 23:48:34 -0500

    Fix loading indicator's missing left-margin

--- webroot/js/main.js
+++ webroot/js/main.js
@@ -7,7 +7,7 @@ function preventMultipleSubmit(formQuery) {
   form.addEventListener('submit', function () {
     const button = this.querySelector('button[type="submit"]');
     const loadingIndicator = document.createElement('i')
-    loadingIndicator.className = 'fa-solid fa-spinner fa-spin-pulse';
+    loadingIndicator.className = 'fa-solid fa-spinner fa-spin-pulse prevent-multiple-submit__loading-indicator';
     loadingIndicator.title = 'Loading';
     button.appendChild(loadingIndicator);
     button.setAttribute('disabled', 'disabled');
