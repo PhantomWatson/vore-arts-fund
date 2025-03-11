b2e85afcf57da0f6fb1d4086e523da445e72d415
Graham Watson <graham@phantomwatson.com>
2025-03-11 04:20:27 -0400

    Fix typo

--- src/Command/EmailDatabaseBackupCommand.php
+++ src/Command/EmailDatabaseBackupCommand.php
@@ -56,7 +56,7 @@ class EmailDatabaseBackupCommand extends Command
         $filepath = ROOT . DS . 'database_backups' . DS . $filename;
 
         $command = sprintf(
-            'mysqldump --user=%s --password=%s --host=%s %s --ignore-table=%.email_queue > %s',
+            'mysqldump --user=%s --password=%s --host=%s --ignore-table=%s.email_queue %s > %s',
             $user,
             $password,
             $host,
