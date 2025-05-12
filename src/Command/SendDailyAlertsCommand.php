e84249076e88d89a2d4f7e48d8e1f5ad68e6d614
Graham Watson <graham@phantomwatson.com>
2025-05-12 02:49:39 -0400

    Add alerts for cycle review, promotion, and awarding prompts

--- src/Command/SendDailyAlertsCommand.php
+++ src/Command/SendDailyAlertsCommand.php
@@ -52,13 +52,15 @@ class SendDailyAlertsCommand extends Command
     {
         $this->fundingCyclesTable = TableRegistry::getTableLocator()->get('FundingCycles');
         $this->alertApplicationPeriodEnded();
+        $this->alertVotingPeriodStarted();
+        $this->alertVotingPeriodEnded();
 
         echo 'Done' . PHP_EOL;
     }
 
     private function alertApplicationPeriodEnded(): void
     {
-        echo 'Checking for cycles with application periods that ended in the last 24 hours' . PHP_EOL;
+        echo 'Checking for cycle with application period that ended in the last 24 hours' . PHP_EOL;
 
         // Look for a funding cycle whose application_end was in the last 24 hours
         $cycle = $this->fundingCyclesTable
@@ -75,11 +77,75 @@ class SendDailyAlertsCommand extends Command
             $alert = new Alert();
             $alert->addLine(
                 sprintf(
-                    'Application period ended for the %s funding cycle. <%s|Time to review applications.>',
+                    'Application period ended for the %s funding cycle. <%s|Time to review applications!>',
                     $cycle->name,
                     Router::url([
                         'prefix' => 'Admin',
                         'controller' => 'Projects',
+                        'action' => 'index',
+                        'id' => $cycle->id
+                    ], true),
+                ),
+            );
+            $alert->send(Alert::TYPE_ADMIN);
+        } else {
+            echo '- No cycles found' . PHP_EOL;
+        }
+    }
+
+    private function alertVotingPeriodStarted()
+    {
+        echo 'Checking for cycle with voting period that started in the last 24 hours' . PHP_EOL;
+
+        // Look for a funding cycle whose vote_begin was in the last 24 hours
+        $cycle = $this->fundingCyclesTable
+            ->find()
+            ->where(function (QueryExpression $exp) {
+                return $exp
+                    ->lt('vote_begin', date('Y-m-d H:i:s'))
+                    ->gt('vote_begin', date('Y-m-d H:i:s', strtotime('-1 day')));
+            })
+            ->first();
+
+        if ($cycle) {
+            echo '- Found cycle, sending alert' . PHP_EOL;
+            $alert = new Alert();
+            $alert->addLine(
+                sprintf(
+                    'Voting period started for the %s funding cycle. Time to promote!',
+                    $cycle->name,
+                ),
+            );
+            $alert->send(Alert::TYPE_ADMIN);
+        } else {
+            echo '- No cycles found' . PHP_EOL;
+        }
+    }
+
+    private function alertVotingPeriodEnded()
+    {
+        echo 'Checking for cycle with voting period that ended in the last 24 hours' . PHP_EOL;
+
+        // Look for a funding cycle whose vote_end was in the last 24 hours
+        $cycle = $this->fundingCyclesTable
+            ->find()
+            ->where(function (QueryExpression $exp) {
+                return $exp
+                    ->lt('vote_end', date('Y-m-d H:i:s'))
+                    ->gt('vote_end', date('Y-m-d H:i:s', strtotime('-1 day')));
+            })
+            ->first();
+
+        if ($cycle) {
+            echo '- Found cycle, sending alert' . PHP_EOL;
+            $alert = new Alert();
+            $alert->addLine(
+                sprintf(
+                    'Voting period ended for the %s funding cycle. <%s|Time to review votes and award loans!>',
+                    $cycle->name,
+                    Router::url([
+                        'prefix' => 'Admin',
+                        'controller' => 'Votes',
                         'action' => 'index',
                         'id' => $cycle->id
                     ], true),
