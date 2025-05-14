f838506086c5fb80879a75b10e8330aae31ac0f4
Graham Watson <graham@phantomwatson.com>
2025-05-14 02:03:42 -0400

    Distinguish awarded and disbursed

--- templates/Admin/Votes/index.php
+++ templates/Admin/Votes/index.php
@@ -39,6 +39,10 @@ function getToAward(Project $project, $budgetRemaining)
         return '<span class="voting-results__award voting-results__award--awarded">'
             . '<i class="fa-solid fa-circle-check"></i> '
             . $amountDisplayed . ' awarded</span>';
+    } elseif ($project->isAwarded()) {
+        return '<span class="voting-results__award voting-results__award--awarded-pending">'
+            . '<i class="fa-solid fa-triangle-exclamation"></i> '
+            . $amountDisplayed . ' awarded, pending loan agreement</span>';
     }
 
     // Mark this project as needing to be funded
