df449a3f1264fbab7aeb3e763047640a1be85fb2
Graham Watson <graham@phantomwatson.com>
2025-07-01 01:15:51 -0400

    Fix typos and tweak language in rejection letter

--- templates/email/html/application_not_funded.php
+++ templates/email/html/application_not_funded.php
@@ -11,7 +11,7 @@
 </p>
 
 <p>
-    Thanks for applying to the Vore Arts Fund! We appreciate the work that you put into your application, your interest in our mission to support the local arts, and the work that you do to contribute to our community. However, we sorry to say that won't be able to provide funding for <strong><?= $project->title ?></strong> in this funding cycle. When this happens, it's due to us running out of budgeted money after tallying the votes and funding the highest-ranked applications. But hey, you can try again in the next funding cycle!
+    Thanks for applying to the Vore Arts Fund! We appreciate the work that you put into your application, your interest in our mission to support the local arts, and the work that you do to contribute to our community. However, we're sorry to say that due to budgetary constraints we won't be able to provide funding for <strong><?= $project->title ?></strong> in this funding cycle. But hey, you're invited to reapply!
 </p>
 
 <p>
