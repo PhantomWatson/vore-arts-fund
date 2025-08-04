3c086ffdbe7b590465089b8f15aad0d6a3dd877b
Graham Watson <graham@phantomwatson.com>
2025-08-04 18:40:38 -0400

    Make URL param name explicit

--- src/Nudges/ReportReminderNudge.php
+++ src/Nudges/ReportReminderNudge.php
@@ -55,7 +55,7 @@ class ReportReminderNudge implements NudgeInterface
                         'plugin' => false,
                         'controller' => 'Reports',
                         'action' => 'submit',
-                        $project->id,
+                        'id' => $project->id,
                     ],
                     true
                 ),
