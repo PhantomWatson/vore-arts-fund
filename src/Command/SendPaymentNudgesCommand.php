82e19a5276ca9d9441e594619d5518c2488d649a
Graham Watson <graham@phantomwatson.com>
2025-08-08 00:34:47 -0400

    Add CLI command for sending payment nudges

---
+++ src/Command/SendPaymentNudgesCommand.php
@@ -0,0 +1,62 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Command;
+
+use App\Nudges\PaymentNudge;
+use Cake\Command\Command;
+use Cake\Console\Arguments;
+use Cake\Console\ConsoleIo;
+use Cake\Console\ConsoleOptionParser;
+
+class SendPaymentNudgesCommand extends Command
+{
+    /**
+     * Hook method for defining this command's option parser.
+     *
+     * @see https://book.cakephp.org/4/en/console-commands/commands.html#defining-arguments-and-options
+     * @param \Cake\Console\ConsoleOptionParser $parser The parser to be defined
+     * @return \Cake\Console\ConsoleOptionParser The built parser.
+     */
+    public function buildOptionParser(ConsoleOptionParser $parser): ConsoleOptionParser
+    {
+        $parser = parent::buildOptionParser($parser);
+
+        return $parser;
+    }
+
+    /**
+     * Creates a DB backup and emails it to the support email address
+     *
+     * @param \Cake\Console\Arguments $args The command arguments.
+     * @param \Cake\Console\ConsoleIo $io The console io
+     * @return void
+     */
+    public function execute(Arguments $args, ConsoleIo $io)
+    {
+        $projects = PaymentNudge::getProjects();
+
+        $io->out(sprintf(
+            'Found %s %s that need payment nudges',
+            $projects->count(),
+            __n('project', 'projects', $projects->count())
+        ));
+
+        if ($projects->isEmpty()) {
+            $io->out('Done');
+            return;
+        }
+
+        foreach ($projects as $project) {
+            $result = PaymentNudge::send($project);
+            if ($result !== true) {
+                $io->error("- $result");
+                continue;
+            }
+
+            $io->success("- Sent report nudge for project #{$project->id}");
+        }
+
+        $io->out('Done');
+    }
+}
