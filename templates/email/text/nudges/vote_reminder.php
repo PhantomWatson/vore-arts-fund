3d771aad743bea7cd1e3ef0c2dc73fa179fbd050
Graham Watson <graham@phantomwatson.com>
2025-12-15 19:53:12 -0500

    Fix variable interpolation

--- templates/email/text/nudges/vote_reminder.php
+++ templates/email/text/nudges/vote_reminder.php
@@ -8,6 +8,6 @@
 
 <?= $userName ?>,
 
-Voting has begun for the current batch of Vore Arts Fund applications, including your project, $projectTitle! Be sure to tell your friends and supporters to visit https://VoreArtsFund.org and cast their votes by <?= $deadline ?>, and remember that you can vote too!
+Voting has begun for the current batch of Vore Arts Fund applications, including your project, <?= $projectTitle ?>! Be sure to tell your friends and supporters to visit https://VoreArtsFund.org and cast their votes by <?= $deadline ?>, and remember that you can vote too!
 
 Best of luck!
